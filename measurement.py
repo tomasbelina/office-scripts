a8cd1ad7409bedc9b3e88e35bc6f35e6b7b038c5
Jiri Vinduska <vinduskajiri@gmail.com>
2022-03-09 08:56:51 +0100

    add import

--- measurement.py
+++ measurement.py
@@ -6,6 +6,7 @@ from weather_repo import addWeather
 import requests
 from dotenv import load_dotenv
 from os.path import join, dirname
+import os
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)

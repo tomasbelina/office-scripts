ebf5cc925d7249170e01a97befc986ea5df44a8c
Jiri Vinduska <vinduskajiri@gmail.com>
2022-03-10 09:46:45 +0100

    print rgb

--- change-color.py
+++ change-color.py
@@ -1,15 +1,20 @@
 from api import engine
 import sqlalchemy
 from settings import Settings
+import sys
 
 Session = sqlalchemy.orm.sessionmaker()
 Session.configure(bind=engine)
 session = Session()
+type = "COLOR_JIRI"
+if(len(sys.argv) == 2 and sys.argv[1] == "Belca"):
+    type = "COLOR_BELCA"
 
-settingsJiri = session.query(Settings).filter(
-    Settings.type == 'COLOR_JIRI').one()
-settingsBelca = session.query(Settings).filter(
-    Settings.type == 'COLOR_BELCA').one()
+settings = session.query(Settings).filter(
+    Settings.type == type).one()
 
-print(settingsJiri.value)
-print(settingsBelca.value)
+print(settings.type)
+print(settings.value)
+color = settings.value.lstrip("#")
+colorRGB = tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
+print('RGB =', colorRGB )

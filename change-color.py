fe3bcf4f35a036d0e79d9a01e01091dec81279d9
Jiri Vinduska <vinduskajiri@gmail.com>
2022-03-10 13:41:27 +0100

    color change

--- change-color.py
+++ change-color.py
@@ -1,9 +1,9 @@
-import board
-import neopixel
+from rpi_ws281x import PixelStrip, Color
 from api import engine
 import sqlalchemy
 from settings import Settings
 import sys
+import time
 
 Session = sqlalchemy.orm.sessionmaker()
 Session.configure(bind=engine)
@@ -16,20 +16,33 @@ settingsBelca = session.query(Settings).filter(
 
 print(settingsJiri.type)
 print(settingsJiri.value)
-color = settingsJiri.value.lstrip("#")
-colorRGB = tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
-print('RGB =', colorRGB)
+colorJiri = settingsJiri.value.lstrip("#")
+colorRGBJiri = tuple(int(colorJiri[i:i+2], 16) for i in (0, 2, 4))
+print('RGB =', colorRGBJiri)
 
 print(settingsBelca.type)
 print(settingsBelca.value)
-color = settingsBelca.value.lstrip("#")
-colorRGB = tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
-print('RGB =', colorRGB)
+colorBelca = settingsBelca.value.lstrip("#")
+colorRGBBelca = tuple(int(colorBelca[i:i+2], 16) for i in (0, 2, 4))
+print('RGB =', colorRGBJiri)
 
-pixel_pin = board.D18
-num_pixels = 150
-ORDER = neopixel.RGB
-pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
-)
-pixels.fill(colorRGB)
+LED_COUNT = 150        # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+# True to invert the signal (when using NPN transistor level shift)
+LED_INVERT = False
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+# Create NeoPixel object with appropriate configuration.
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA,
+                   LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+# Intialize the library (must be called once before other functions).
+strip.begin()
+
+for i in range(strip.numPixels()):
+    strip.setPixelColor(i, colorJiri)
+    strip.show()
+    time.sleep(50 / 1000.0)

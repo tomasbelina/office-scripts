4a1d87022a6177c0d45f43e7d1602bbd1736f624
Jiri Vinduska <vinduskajiri@gmail.com>
2022-03-10 14:35:16 +0100

    sleep change

--- change-color.py
+++ change-color.py
@@ -46,7 +46,7 @@ try:
                 strip.setPixelColor(
                     i, Color(colorRGBBelca[0], colorRGBBelca[1], colorRGBBelca[2]))
                 strip.show()
-        time.sleep(10)
+        time.sleep(1)
 except KeyboardInterrupt:
     for i in range(0, 150):
         strip.setPixelColor(

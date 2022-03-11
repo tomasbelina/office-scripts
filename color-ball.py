ba27f2a73927c3436836a986b8ec828dc15b5957
Tomáš Bělina <belina@tri.cz>
2022-03-11 12:48:46 +0100

    another fckup

--- color-ball.py
+++ color-ball.py
@@ -6,13 +6,15 @@ ball_color = (0, 0, 255)
 previous_color = None
 
 strip.begin()
+strip.show()
+
 for pixel_index in range(LED_START, LED_COUNT):
     if previous_color:
         strip.setPixelColor(pixel_index - 1, previous_color)
     previous_color = strip.getPixelColor(pixel_index)
     strip.setPixelColor(pixel_index, Color(*ball_color))
     strip.show()
-    time.sleep(0.5)
+    time.sleep(0.25)
 
 if previous_color:
     strip.setPixelColor(LED_COUNT - 1, previous_color)

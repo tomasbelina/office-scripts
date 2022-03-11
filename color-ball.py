311449dc3c1a37359078750f18b12cdaed351a5a
Tomáš Bělina <belina@tri.cz>
2022-03-11 12:51:05 +0100

    now the ball is really working

--- color-ball.py
+++ color-ball.py
@@ -2,10 +2,14 @@ from rpi_ws281x import Color
 from color import strip, LED_COUNT, LED_MIDDLE, LED_START
 import time
 
+strip_color = (255, 0, 0)
 ball_color = (0, 0, 255)
 previous_color = None
 
 strip.begin()
+
+for pixel_index in range(LED_START, LED_COUNT):
+    strip.setPixelColor(pixel_index, Color(*strip_color))
 strip.show()
 
 for pixel_index in range(LED_START, LED_COUNT):
@@ -14,7 +18,7 @@ for pixel_index in range(LED_START, LED_COUNT):
     previous_color = strip.getPixelColor(pixel_index)
     strip.setPixelColor(pixel_index, Color(*ball_color))
     strip.show()
-    time.sleep(0.25)
+    time.sleep(0.1)
 
 if previous_color:
     strip.setPixelColor(LED_COUNT - 1, previous_color)

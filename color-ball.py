91e570044bef27bb45b8cd20e3b9c074980f5fd4
Tomáš Bělina <belina@tri.cz>
2022-03-11 13:13:15 +0100

    strip harakiri

--- color-ball.py
+++ color-ball.py
@@ -1,24 +1,11 @@
 from rpi_ws281x import Color
-from color import strip, LED_COUNT, LED_MIDDLE, LED_START
+from color import OfficeStrip, LED_COUNT, LED_MIDDLE, LED_START
 import time
 
 strip_color = (255, 0, 0)
 ball_color = (0, 0, 255)
 previous_color = None
 
-strip.begin()
-
-for pixel_index in range(LED_START, LED_COUNT):
-    strip.setPixelColor(pixel_index, Color(*strip_color))
-strip.show()
-
-for pixel_index in range(LED_START, LED_COUNT):
-    if previous_color:
-        strip.setPixelColor(pixel_index - 1, previous_color)
-    previous_color = strip.getPixelColor(pixel_index)
-    strip.setPixelColor(pixel_index, Color(*ball_color))
-    strip.show()
-    time.sleep(0.1)
-
-if previous_color:
-    strip.setPixelColor(LED_COUNT - 1, previous_color)
+strip =  OfficeStrip()
+strip.set_desk_colors()
+strip.send_ball()
--- color.py
+++ color.py
@@ -1,4 +1,8 @@
-from rpi_ws281x import PixelStrip
+from rpi_ws281x import PixelStrip, Color
+from api import engine
+import sqlalchemy
+from settings import Settings
+import time
 
 LED_START = 0
 LED_MIDDLE = 87
@@ -12,7 +16,46 @@ LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-# Create NeoPixel object with appropriate configuration.
-strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA,
-                   LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+class  v:
 
+    def __init__(self):
+        self._strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA,
+                                    LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+        self._strip.begin()
+
+    def set_desk_colors(self):
+        Session = sqlalchemy.orm.sessionmaker()
+        Session.configure(bind=engine)
+        session = Session()
+
+        settingsJiri = session.query(Settings).filter(
+            Settings.type == "COLOR_JIRI").one()
+        settingsBelca = session.query(Settings).filter(
+            Settings.type == "COLOR_BELCA").one()
+        colorJiri = settingsJiri.value.lstrip("#")
+        colorRGBJiri = tuple(int(colorJiri[i:i+2], 16) for i in (0, 2, 4))
+        colorBelca = settingsBelca.value.lstrip("#")
+        colorRGBBelca = tuple(
+            int(colorBelca[i:i+2], 16) for i in (0, 2, 4))
+
+        for i in range(LED_START, LED_MIDDLE):
+            self._strip.setPixelColor(
+                i, Color(*colorRGBJiri))
+
+        for i in range(LED_MIDDLE, LED_COUNT):
+            self._strip.setPixelColor(
+                i, Color(*colorRGBBelca))
+        self._strip.show()
+
+    def send_ball(self, ball_color=(0, 0, 255)):
+        previous_color = None
+        for pixel_index in range(LED_START, LED_COUNT):
+            if previous_color:
+                self._strip.setPixelColor(pixel_index - 1, previous_color)
+            previous_color = self._strip.getPixelColor(pixel_index)
+            self._strip.setPixelColor(pixel_index, Color(*ball_color))
+            self._strip.show()
+            time.sleep(0.1)
+
+        if previous_color:
+            self._strip.setPixelColor(LED_COUNT - 1, previous_color)
